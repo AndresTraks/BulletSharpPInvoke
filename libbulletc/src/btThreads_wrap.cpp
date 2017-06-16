015477cc4b20a57f41e0db5c0380b87d422c99b1
AndresTraks <andres.traks@gmail.com>
2017-06-16 21:14:10 +0300

    Add missing btThreads changes

--- libbulletc/src/btThreads_wrap.cpp
+++ libbulletc/src/btThreads_wrap.cpp
@@ -2,6 +2,26 @@
 
 #include "btThreads_wrap.h"
 
+int btITaskScheduler_getMaxNumThreads(btITaskScheduler* obj)
+{
+	return obj->getMaxNumThreads();
+}
+
+const char* btITaskScheduler_getName(btITaskScheduler* obj)
+{
+	return obj->getName();
+}
+
+int btITaskScheduler_getNumThreads(btITaskScheduler* obj)
+{
+	return obj->getNumThreads();
+}
+
+void btITaskScheduler_setNumThreads(btITaskScheduler* obj, int numThreads)
+{
+	obj->setNumThreads(numThreads);
+}
+
 btITaskScheduler* btThreads_btGetSequentialTaskScheduler()
 {
 	return btGetSequentialTaskScheduler();
--- libbulletc/src/btThreads_wrap.h
+++ libbulletc/src/btThreads_wrap.h
@@ -3,6 +3,11 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+	EXPORT int btITaskScheduler_getMaxNumThreads(btITaskScheduler* obj);
+	EXPORT const char* btITaskScheduler_getName(btITaskScheduler* obj);
+	EXPORT int btITaskScheduler_getNumThreads(btITaskScheduler* obj);
+	EXPORT void btITaskScheduler_setNumThreads(btITaskScheduler* obj, int numThreads);
+
 	EXPORT btITaskScheduler* btThreads_btGetSequentialTaskScheduler();
 	EXPORT btITaskScheduler* btThreads_btGetOpenMPTaskScheduler();
 	EXPORT btITaskScheduler* btThreads_btGetPPLTaskScheduler();
