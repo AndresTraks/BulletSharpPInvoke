b6963c4950182bd83e422734820b71dbeed4a1f1
Eideren <the.eideren@gmail.com>
2018-12-16 05:37:50 +0100

    Update KinematicCharacterController to latest bullet

--- BulletSharp/Dynamics/ICharacterController.cs
+++ BulletSharp/Dynamics/ICharacterController.cs
@@ -4,17 +4,17 @@ namespace BulletSharp
 {
 	public interface ICharacterController : IAction
 	{
-        bool CanJump { get; }
-        bool OnGround { get; }
-
-        void Jump();
-        void Jump(Vector3 dir);
-        void PlayerStep(CollisionWorld collisionWorld, double deltaTime);
-        void PreStep(CollisionWorld collisionWorld);
-        void Reset(CollisionWorld collisionWorld);
-        void SetUpInterpolate(bool value);
-        void SetVelocityForTimeInterval(ref Vector3 velocity, double timeInterval);
         void SetWalkDirection(ref Vector3 walkDirection);
+        void SetVelocityForTimeInterval(ref Vector3 velocity, double timeInterval);
+        void Reset(CollisionWorld collisionWorld);
         void Warp(ref Vector3 origin);
+        
+        void PreStep(CollisionWorld    collisionWorld);
+        void PlayerStep(CollisionWorld collisionWorld, double deltaTime);
+        bool CanJump { get; }
+        void Jump(Vector3? dir = null);
+        
+        bool OnGround { get; }
+        void SetUpInterpolate(bool value);
 	}
 }
